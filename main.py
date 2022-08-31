c16dce23182b81b47d1133be6f4f15e8c72ef2a6
j-theripper <patel.jbp97@gmail.com>
2022-08-31 17:02:36 +0530

    media api added

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import os
+from dotenv import load_dotenv
 from pysmwrapper import WhatsApp
 
+load_dotenv()
+token = os.getenv("TOKEN")
+print(token)
+whatsapp = WhatsApp(os.getenv("TOKEN"), wa_id=os.getenv("WA_ID"))
 
-whatsapp = WhatsApp('token','919405235423')
-
-hi = whatsapp.send_message('hi','919405235423')
+print(whatsapp.send_media('https://i.imgur.com/FXvlGEd.jpeg','919405235423','image',caption='this').text)
--- pysmwrapper/whatsapp.py
+++ pysmwrapper/whatsapp.py
@@ -6,7 +6,7 @@ import requests
 
 logger = logging.getLogger(__name__)
 formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(name)s:%(message)s")
-file_handler = logging.FileHandler("log/whatsapp_api.log")
+file_handler = logging.FileHandler("pysmwrapper/log/whatsapp_api.log")
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
@@ -39,11 +39,11 @@ class WhatsApp:
 
     def send_message(self,message, phone_number,preview_url=True, recipient_type="individual"):
         """
-        Send text simple message to phone_number
+        Send text simple message to phone_number.
         :params
-            message[str] - Message to send.
-            phone_number[str] - Phone number of user with country code without '+'.
-            preview_url[bool] - Whether to send links with preview or not.
+            message[str]        - Message to send.
+            phone_number[str]   - Phone number of user with country code without '+'.
+            preview_url[bool]   - Whether to send links with preview or not.
             recipient_type[str] - Type of recipient group or individual
         :return
             json response of the request made.
@@ -60,14 +60,70 @@ class WhatsApp:
                             "body": message
                         },
                     }
-            resp = requests.post(url=f"{self.url}", headers=self.headers, json=data, timeout=10)
-        except requests.exceptions.HTTPError as errh:
-            logger.error("Http Error: %s",(errh))
-        except requests.exceptions.ConnectionError as errc:
-            logger.error("Error Connecting: %s",(errc))
-        except requests.exceptions.Timeout as errt:
-            logger.error("Timeout Error: %s",(errt))
-        except requests.exceptions.RequestException as err:
-            logger.error("OOps: Something Else %s",(err))
+            resp = requests.post(url=self.url, headers=self.headers, json=data, timeout=10)
+        except Exception as err:
+            logger.error("Error in send message: %s",str(err))
         else:
             return resp
+
+    def send_media (self,
+                    media_link,
+                    phone_number,
+                    media_type,
+                    access_type=True,
+                    caption=None,
+                    recipient_type="individual"
+                    ):
+        """
+        Send media message(audio, document, image, video) to phone_number.
+        :params
+            media_type[str]   - Type of media(audio, document, image, video) to send.
+            phone_number[str] - Phone number of user with country code without '+'.
+            media_link[str]   - Link of media if access_type is True or Media ID if access_type
+                                is False
+            access_type[bool] - True means send media with media_link, False means send media
+                                with id.
+            caption[str]      - Caption of the media.
+        """
+        resp = None
+        try:
+            data = self._get_media_payload(media_link,phone_number,media_type,access_type,
+                                            recipient_type)
+            if media_type != "audio":
+                data[media_type]["caption"] = caption
+
+            resp = requests.post(self.url, headers=self.headers, json=data)
+        except Exception as err:
+            logger.error("Error in send message: %s",(err))
+        else:
+            return resp
+
+
+    def _get_media_payload (self,
+                            media_link,
+                            phone_number,
+                            media_type,
+                            access_type=True,
+                            recipient_type="individual"
+                            ):
+        if access_type:
+            return {
+                        "messaging_product": "whatsapp",
+                        "recipient_type": recipient_type,
+                        "to": phone_number,
+                        "type": media_type,
+                        media_type: {
+                            "link" : media_link,
+                        }
+                    }
+        else:
+            # Payload with media id.
+            return {
+                        "messaging_product": "whatsapp",
+                        "recipient_type": recipient_type,
+                        "to": phone_number,
+                        "type": media_type,
+                        media_type: {
+                            "id" : media_link,
+                        }
+                    }
